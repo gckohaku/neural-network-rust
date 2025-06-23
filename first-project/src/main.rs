bce05be8554c0e616db3201834a68e888aaa4043
gckohaku <51472484+gckohaku@users.noreply.github.com>
2025-06-23 07:49:03 +0000

    Add ndarray and ndarray-linalg dependencies; implement matrix multiplication in main.rs
    - Created a new .vscode/settings.json file to disable auto-saving.
    - Updated Cargo.lock with new dependencies including ndarray and ndarray-linalg.
    - Modified Cargo.toml to include ndarray and ndarray-linalg with specific versions and features.
    - Updated main.rs to perform matrix multiplication using ndarray and ndarray-linalg.

--- first-project/src/main.rs
+++ first-project/src/main.rs
@@ -1,3 +1,9 @@
+use ndarray::*;
+use ndarray_linalg::*;
+
 fn main() {
-    println!("Hello, world!");
+    let a: Array2<i64> = arr2(&[[1, 2, 3], [4, 5, 6], [7, 8, 9]]);
+    let b: Array2<i64> = arr2(&[[9, 8, 7], [6, 5, 4], [3, 2, 1]]);
+
+    println!("resurt: {}", a.dot(&b));
 }
