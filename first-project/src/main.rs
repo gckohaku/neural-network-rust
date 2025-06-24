5c8b04042d7abfd75ffeada266cc36ec7ae18f8c
gckohaku <51472484+gckohaku@users.noreply.github.com>
2025-06-24 07:23:07 +0000

    Configure Rust project settings and update dependencies
    - Updated VSCode settings for Rust development, including auto-save and formatting options.
    - Cleaned up Cargo.lock by removing unnecessary package entries.
    - Modified Cargo.toml to adjust optimization levels for development and release profiles.
    - Simplified main.rs to print "Hello, world!" and removed unused ndarray and ndarray_linalg imports.
    - Added a new Cargo configuration file to specify linker arguments for Linux targets.
    - Created an empty matrix.rs file for future implementation.

--- first-project/src/main.rs
+++ first-project/src/main.rs
@@ -1,9 +1,5 @@
-use ndarray::*;
-use ndarray_linalg::*;
-
 fn main() {
-    let a: Array2<i64> = arr2(&[[1, 2, 3], [4, 5, 6], [7, 8, 9]]);
-    let b: Array2<i64> = arr2(&[[9, 8, 7], [6, 5, 4], [3, 2, 1]]);
-
-    println!("resurt: {}", a.dot(&b));
+	println!("Hello, world!");
+	// This is a simple Rust program that prints "Hello, world!" to the console.
+	// You can run this program using `cargo run` in your terminal.
 }
