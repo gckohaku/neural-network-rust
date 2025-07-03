bffee6bcd4626351afcfb504d55dcc8fb246e7c3
gckohaku <51472484+gckohaku@users.noreply.github.com>
2025-07-03 08:01:04 +0000

    マクロを追加し、Matrix構造体の加算操作を実装。main関数での呼び出しを更新。

--- first-project/src/main.rs
+++ first-project/src/main.rs
@@ -1,11 +1,14 @@
-use crate::neural_network::NeuralNetwork;
+use std::ops;
+
+use crate::{macros::apply_ops::apply_arithmetics, matrix::Matrix, neural_network::NeuralNetwork};
 
 mod matrix;
 mod neural_network;
 mod auto_apply_ops;
+mod macros;
 
 fn main() {
 	let nn = NeuralNetwork::new(vec![3, 2, 1]);
 	
-	println!("{:#?}", nn);
+	apply_arithmetics_base!(Matrix, Matrix, ops::Add, add, +);
 }
