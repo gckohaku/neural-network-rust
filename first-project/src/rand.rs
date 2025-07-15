f0036dc90b49240cce39dea296234ee518b57856
gckohaku <51472484+gckohaku@users.noreply.github.com>
2025-07-15 07:26:45 +0000

    Rand構造体のフィールドを修正し、乱数生成のためのxoroshiro128**アルゴリズムの実装に向けた準備を整えました。

--- first-project/src/rand.rs
+++ first-project/src/rand.rs
@@ -2,23 +2,40 @@ use std::time::{SystemTime, UNIX_EPOCH};
 
 #[derive(Clone, Debug)]
 pub struct Rand {
-	pub seed: u64,
+    x: u64,
+    y: u64,
 }
 
-// TODO: xorshift を使って乱数を生成する
+fn rotl(x: u64, k: u8) -> u64 {
+    (&x << &k) | (&x >> (64 - &k))
+}
+
+// TODO: xoroshiro128** を使って乱数を生成する
 
 impl Rand {
-	pub fn new() -> Self {
-		Self {
-			seed: SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_secs(),
-		}
-	}
+    pub fn new(&mut self) -> Self {
+        let seed = SystemTime::now()
+            .duration_since(UNIX_EPOCH)
+            .unwrap()
+            .as_secs();
 
-	pub fn new_with_seed(seed: u64) -> Self {
-		Self { seed }
-	}
+        Rand::new_with_seed(seed)
+    }
 
-	pub fn set_seed(&mut self, seed: u64) {
-		self.seed = seed;
+    pub fn new_with_seed(seed: u64) -> Self {
+        Self {
+            x: seed,
+            y: rotl(rotl(seed, 32), 37),
+        }
+    }
+
+    pub fn set_seed(&mut self, seed: u64) {
+        self.x = seed;
+		self.y = rotl(rotl(seed, 32), 37);
+    }
+
+	// TODO: xoroshiro128** での乱数の生成
+	pub fn next() {
+
 	}
 }
