2b12a65404fff8c2cb8046935bd2bf5089e8c80b
gckohaku <51472484+gckohaku@users.noreply.github.com>
2025-07-22 07:29:43 +0000

    WIP #4

--- first-project/src/main.rs
+++ first-project/src/main.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use crate::{matrix::Matrix, neural_network::NeuralNetwork, rand::Rand};
 
 mod matrix;
@@ -14,9 +16,42 @@ fn main() {
 
     let mut r = Rand::new();
 
-    for _ in 0 .. 50 {
-        println!("{:.20}", r.rand_f64());
-    }
+    // 複数回乱数を回してテスト
+    // for _ in 0 .. 50 {
+    //     println!("{:.20}", r.rand_f64());
+    // }
 
     // TODO: 正規分布に従った乱数の生成がしっかりと実装できているかをテスト
+    let division_values = 20;
+    let min_value = 0.0;
+    let max_value = 1.0;
+    let value_range = max_value - min_value;
+    let values_length = 100000;
+    let mut counts = HashMap::new();
+
+    for i in 0 .. division_values {
+        let key = format!("{:.2} <= v < {:.2}", min_value + (value_range / division_values as f64) * i as f64, min_value + (value_range / division_values as f64) * (i + 1) as f64);
+        println!("{}", key);
+
+        counts.insert(key, 0);
+    }
+
+    for _i in 0 .. values_length {
+        let value = r.rand_f64();
+        println!("{:.20}", value);
+        println!("{:.20}", (value_range / division_values as f64));
+
+        let category = (((value - min_value) * division_values as f64) / division_values as f64).ceil();
+
+        let key = format!("{:.2} <= v < {:.2}", min_value + (value_range / division_values as f64) * category, min_value + (value_range / division_values as f64) * (category + 1.0));
+        println!("{}", key);
+
+        let new_value = counts.get(&key).unwrap() + 1;
+
+        counts.insert(key, new_value);
+    }
+
+    for data in counts {
+        println!("{:?}", data)
+    }
 }
