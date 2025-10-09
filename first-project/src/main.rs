b06837062215d23bda5fa046bbb1d0fbfa24f5f1
gckohaku <cyanhairneko@gmail.com>
2025-10-10 00:58:45 +0900

    行列積実行時間計測チェリーピック用

--- first-project/src/main.rs
+++ first-project/src/main.rs
@@ -11,14 +11,9 @@ mod cpu_info;
 mod matrix_mul_calc_speed;
 
 fn main() {
-    iris_nn_process();
+    // iris_nn_process();
 
     // iris_analyze();
 
-    // let mut m1 = Matrix::new_from_vec(3, 3, vec![1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0]).unwrap();
-    // let m2 =  Matrix::new_from_vec(3, 3, vec![3.0, 4.0, 6.0, 4.0, 2.0, 5.0, 3.0, 1.0, 4.0]).unwrap();
-
-    // m1 *= &m2;
-
-    // println!("{:7.1}", m1);
+    matrix_mul_calc_speed::calc();
 }
