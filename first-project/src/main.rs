111c45d473a16011ee2d0d9e0182ff3ed0047219
gckohaku <cyanhairneko@gmail.com>
2025-11-05 16:00:37 +0900

    検証データの数とテストデータの数を同数に

--- first-project/src/main.rs
+++ first-project/src/main.rs
@@ -24,13 +24,13 @@ fn main() {
     let epochs = 20;
     let learning_chunk_size = 250;
     let mini_batch_iteration = 16;
-    let validation_iteration = 30;
-    let test_iteration = 4;
+    let validation_iteration = 10;
+    let test_iteration = 10;
     let training_max_value = 60000;
-    let validation_max_value = 9000;
-    let test_max_value = 1000;
-    let validation_chunk_size = 300;
-    let test_chunk_size = 250;
+    let validation_max_value = 5000;
+    let test_max_value = 5000;
+    let validation_chunk_size = 500;
+    let test_chunk_size = 500;
 
     // mnist_process();
     mnist_nn_mt::mnist_process(epochs, learning_chunk_size, mini_batch_iteration, validation_iteration, test_iteration, training_max_value, validation_max_value, test_max_value, validation_chunk_size, test_chunk_size);
