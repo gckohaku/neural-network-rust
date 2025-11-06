b02e52d60b25747619d6df55ad0bf0401559f429
gckohaku <cyanhairneko@gmail.com>
2025-11-06 15:16:06 +0900

    シグモイド関数と、その導関数を定義

--- first-project/src/neural_network_functions.rs
+++ first-project/src/neural_network_functions.rs
@@ -16,6 +16,14 @@ pub fn differential_leaky_relu(x: &f64) -> f64 {
     if *x > 0.0 { 1.0 } else { 0.01 }
 }
 
+pub fn sigmoid(x: &f64) -> f64 {
+    ((x / 2.0).tanh() + 1.0) / 2.0
+}
+
+pub fn differential_sigmoid(x: &f64) -> f64 {
+    sigmoid(x) * (1.0 - sigmoid(x))
+}
+
 pub fn softmax(z: &mut Matrix) {
     let nodes_exp_sum: f64 = z.data.iter().map(|x| x.exp()).sum();
     for i in 0..z.rows {
